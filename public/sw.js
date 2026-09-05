237ae61a21671f9fe61c17dcc9ec5af9d7954a56
Karute <24kurasu2@gmail.com>
2026-09-05 21:03:10 +0900

    sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE = "bj-online-v4";
-const ASSETS = ["/", "/index.html", "/style.css", "/client.js", "/manifest.webmanifest", "/bgm.wav"];
+const ASSETS = ["/", "/index.html", "/style.css", "/client.js", "/manifest.webmanifest", "/bgm.mp3"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
 self.addEventListener("fetch", e => { if (e.request.headers.get("upgrade") === "websocket") return; e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
