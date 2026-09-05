ed409895d72b7b3e95ce5992bd53e54ca1bc57b3
Karute <24kurasu2@gmail.com>
2026-09-05 19:39:27 +0900

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+const CACHE = "bj-online-v2";
+const ASSETS = ["/", "/index.html", "/style.css", "/client.js", "/manifest.webmanifest"];
+self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+self.addEventListener("fetch", e => {
+  if (e.request.headers.get("upgrade") === "websocket") return;
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
